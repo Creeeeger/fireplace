773637dc5503673226035b136ee63264c79b8b87
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2026-07-13 11:04:00 +0200

    refactor: simplify SPEEDY callbacks

--- emulator/soc/speedy/speedy.c
+++ emulator/soc/speedy/speedy.c
@@ -23,8 +23,13 @@
 
 #include <fireplace/soc/speedy/speedy.h>
 
-char speedy_0_slave_reg[0x100] = "\x0";
-char speedy_1_slave_reg[0x100] = "\x0";
+#define S2MPS19_STATUS1_REG 0x0a
+#define S2MPS19_STATUS1_JIGONB (1U << 1)
+
+static uint8_t speedy_0_slave_reg[0x100];
+static uint8_t speedy_1_slave_reg[0x100];
+static uint8_t speedy_0_reg;
+static uint8_t speedy_1_reg;
 
 bool speedy_0_en = true;
 bool speedy_1_en = true;
@@ -38,23 +43,42 @@ pthread_mutex_t speedy_lock = PTHREAD_MUTEX_INITIALIZER;
 int speedy_init(struct uc_struct *uc_s)
 {
 	uint32_t speedy_initial_state = 1;
+	uc_err err;
 
-	printf("= speedy_init\n");
+	/* JIGONB is active-low; leave the emulated JIG input deasserted. */
+	speedy_0_slave_reg[S2MPS19_STATUS1_REG] =
+		S2MPS19_STATUS1_JIGONB;
 
-	uc_mem_write(uc_s, 0x15940000 + SPEEDY_CTRL, &speedy_initial_state, 0x4);
-        uc_mem_write(uc_s, 0x15950000 + SPEEDY_CTRL, &speedy_initial_state, 0x4);
-
-	return 0;
+	err = uc_mem_write(uc_s, 0x15940000 + SPEEDY_CTRL,
+			   &speedy_initial_state, sizeof(speedy_initial_state));
+	if (err == UC_ERR_OK)
+		err = uc_mem_write(uc_s, 0x15950000 + SPEEDY_CTRL,
+				   &speedy_initial_state,
+				   sizeof(speedy_initial_state));
+	return err;
 }
 
 void speedy_hook(uc_engine *uc, uc_mem_type type, uint64_t address, int size, int64_t value, void *user_data)
 {
+	uint32_t val;
+	uint8_t *slave_regs;
+	uint8_t *selected_reg;
+	uc_err err = UC_ERR_OK;
+
 	pthread_mutex_lock(&speedy_lock);
 
 	if(address >= 0x15950000)
 		base = 0x15950000;
 	else
 		base = 0x15940000;
+
+	if (base == 0x15940000) {
+		slave_regs = speedy_0_slave_reg;
+		selected_reg = &speedy_0_reg;
+	} else {
+		slave_regs = speedy_1_slave_reg;
+		selected_reg = &speedy_1_reg;
+	}
 
 	if(type == UC_MEM_WRITE)
 	{
@@ -68,14 +92,6 @@ void speedy_hook(uc_engine *uc, uc_mem_type type, uint64_t address, int size, in
 					else
 						speedy_1_en = true;
 
-                                        printf("SPEEDY %i now enabled!\n",
-					       address >= 0x15950000 ? 1 : 0);
-				}
-
-				if ((value >> 31) & 1)
-				{
-					printf("SWRESET Requested on SPEEDY %i\n",
-					       address >= 0x15950000 ? 1 : 0);
 				}
 				break;
 
@@ -84,12 +100,25 @@ void speedy_hook(uc_engine *uc, uc_mem_type type, uint64_t address, int size, in
 				{
 					uint32_t val = 0x0;
 
-					printf("FIFO RESET Requested on SPEEDY %i\n",
-					       address >= 0x15950000 ? 1 : 0);
-
-					uc_mem_write(uc, address, &val, 0x4);
-					uc_mem_write(uc, base + SPEEDY_FIFO_STATUS, &val, 0x4);
+					err = uc_mem_write(uc, address, &val, sizeof(val));
+					if (err == UC_ERR_OK)
+						err = uc_mem_write(uc,
+							   base + SPEEDY_FIFO_STATUS,
+							   &val, sizeof(val));
 				}
+				break;
+
+			case SPEEDY_CMD:
+				*selected_reg = (uint8_t)((value >> 7) & 0xff);
+				if (!(value & SPEEDY_DIRECTION_WRITE)) {
+					val = slave_regs[*selected_reg];
+					err = uc_mem_write(uc, base + SPEEDY_RX_DATA,
+							   &val, sizeof(val));
+				}
+				break;
+
+			case SPEEDY_TX_DATA:
+				slave_regs[*selected_reg] = (uint8_t)value;
 				break;
 		}
 	}
@@ -98,18 +127,22 @@ void speedy_hook(uc_engine *uc, uc_mem_type type, uint64_t address, int size, in
 		switch(address - base)
 		{
 			case SPEEDY_CTRL:
-				uint32_t val = 0x1;
+				val = 0x1;
 
-				uc_mem_write(uc, address, &val, 0x4);
+				err = uc_mem_write(uc, address, &val, sizeof(val));
 				break;
 
 			case SPEEDY_FIFO_STATUS:
 				val = 1 << 6;
 
-				printf("Probably wants FIFO empty, just writing it.\n");
-				uc_mem_write(uc, address, &val, 0x4);
+				err = uc_mem_write(uc, address, &val, sizeof(val));
 				break;
 		}
 	}
 	pthread_mutex_unlock(&speedy_lock);
+	if (err != UC_ERR_OK) {
+		fprintf(stderr, "Failed to update SPEEDY state: %s\n",
+			uc_strerror(err));
+		uc_emu_stop(uc);
+	}
 }
